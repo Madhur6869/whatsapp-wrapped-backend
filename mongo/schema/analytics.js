ebd97615024fdec4a6fd1b7cf99e7dc2eef473a3
Madhur6869 <madhur.padavala2002@gmail.com>
2024-01-11 16:10:09 -0500

    Added schema

--- mongo/schema/analytics.js
+++ mongo/schema/analytics.js
@@ -3,8 +3,62 @@ const mongoose = require('mongoose');
 const analyticsSchema = new mongoose.Schema({
     uid:{
         type:String
-    }
-   
+    },
+    isGroup:{
+        type:Boolean
+    },
+    most_active:{
+        name:{
+            type:String
+        },
+        messages:{
+            type:Number
+        }
+    },
+    emojis:{
+        // hex:count
+        type:Object
+    },
+    most_active_day:{
+        Mon:{
+            type:Number,
+            default:0
+        },
+        Tue:{
+            type:Number,
+            default:0
+        },
+        Wed:{
+            type:Number,
+            default:0
+        },
+        Thu:{
+            type:Number,
+            default:0
+        },
+        Fri:{
+            type:Number,
+            default:0
+        },
+        Sat:{
+            type:Number,
+            default:0
+        },
+        Sun:{
+            type:Number,
+            default:0
+        },
+        
+    },
+    average_response_time:{
+        type:String
+    },
+    average_msgs_per_day:{
+        type:Number
+    },
+    total_messages:{
+        type:Number
+    }   
 
 });
 
--- server.js
+++ server.js
@@ -10,6 +10,54 @@ dotenv.config({ path: "./.env" });
 
 dbconnect();
 
+const fs = require('fs');
+
+// function readTextFile(filePath) {
+//   // Read the file asynchronously
+//   fs.readFile(filePath, 'utf8', (err, data) => {
+//     if (err) {
+//       console.error('Error reading the file:', err);
+//       return;
+//     }
+
+//     // Split the contents into an array of lines
+//     const lines = data.split('\n');
+
+//     // Print each line
+//     lines.forEach((line) => {
+//       console.log(line);
+//     });
+//   });
+// }
+
+// function printEmojiHexCodes(filePath) {
+//     // Read the file asynchronously
+//     fs.readFile(filePath, 'utf8', (err, data) => {
+//       if (err) {
+//         console.error('Error reading the file:', err);
+//         return;
+//       }
+  
+//       // Use a regular expression to find emojis in the text
+//       const emojiRegex = /[\uD800-\uDBFF][\uDC00-\uDFFF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDE4F\uDE80-\uDEFF]/g;
+//       const emojis = data.match(emojiRegex);
+  
+//       if (emojis) {
+//         // Print hex codes of emojis
+//         emojis.forEach((emoji) => {
+//           const hexCode = Array.from(emoji).map((char) => char.codePointAt(0).toString(16)).join(' ');
+//           console.log(`Emoji: ${emoji}, Hex Code: ${hexCode}`);
+//         });
+//       } else {
+//         console.log('No emojis found in the text file.');
+//       }
+//     });
+//   }
+
+//   printEmojiHexCodes('public/sampleChat.txt');
+// // Example usage: replace 'your/file/path.txt' with the actual path to your text file
+// // readTextFile('public/sampleChat.txt');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.text());
